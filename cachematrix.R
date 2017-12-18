b24a7c2dc00e580f0ccfdd90f0d51fc4d2a2df53
Trever Sanborn <trever.sanborn@parivedasolutions.com>
2017-12-17 22:39:51 -0600

    create the makeCacheMatrix function

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,19 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## Returns list with four functions that read and write a matrix,
+## as well as read and write the inverse of that matrix.
+makeCacheMatrix <- function(mx = matrix()) {
+    inverse <- NULL
+    set <- function(m) {
+        mx <<- m
+        inverse <<- NULL
+    }
+    get <- function() mx
+    set_inverse <- function(i) inverse <<- i
+    get_inverse <- function() inverse
+    
+    list(set = set, get = get, set_inverse = set_inverse, get_inverse = get_inverse)
 }
 
 
